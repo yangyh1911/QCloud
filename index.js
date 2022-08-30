506e07cb95bc17f258e66f574b90efc80a7a6d74
yangyuanhang <yangyuanhang1@faw.com.cn>
2022-08-30 17:02:23 +0800

    feat: 腾讯桶上传命令行封装，初始化

---
+++ index.js
@@ -0,0 +1,28 @@
+const { Command } = require("commander");
+
+const program = new Command();
+const uploadQCloud = require("qcloud-upload");
+
+program
+  .name("q-cloud cli tool")
+  .description("CLI to q-cloud upload")
+  .version("0.1.0");
+
+program
+  .command("upload")
+  .description("腾讯桶资源上传命令行工具")
+  .option("--SecretId <string>", "SecretId")
+  .option("--SecretKey <string>", "SecretKey")
+  .option("--Bucket <string>", "Bucket")
+  .option("--Region <string>", "Region")
+  .option("--prefix <string>", "prefix")
+  .option("--overWrite <boolean>", "overWrite", false)
+  .option("--src <string>", "src")
+  // .option("--AppId", "AppId")
+  // .option("--Headers", "Headers")
+  .action((str, options) => {
+    console.log("options Bucket", options._optionValues);
+    uploadQCloud(options._optionValues);
+  });
+
+program.parse();
